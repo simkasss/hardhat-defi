7f452edaf9fb056c6026beadcbfceec7947168d5
Simona Kastantinavičiūtė <simona@Simonas-MacBook-Pro.local>
2023-01-16 10:54:22 +0200

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,58 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-deploy")
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
+        },
+        localhost: {
+            chainId: 31337,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            },
+            {
+                version: "0.6.12",
+            },
+            {
+                version: "0.4.19",
+            },
+            {
+                version: "0.6.6",
+            },
+            {
+                version: "0.6.0",
+            },
+        ],
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+    },
+}
---
+++ helper-hardhat-config.js
@@ -0,0 +1,10 @@
+const networkConfig = {
+    31337: {
+        name: "localhost",
+        wethToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        lendingPoolAddressesProvider: "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        daiEthPriceFeed: "0x773616E4d11A78F511299002da57A0a94577F1f4",
+        daiToken: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    },
+}
+module.exports = { networkConfig }
